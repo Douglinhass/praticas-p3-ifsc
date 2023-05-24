660d199d35f6b3da39ee02569b66f33f3b6b24b0
Douglinhass <124809067+Douglinhass@users.noreply.github.com>
2023-05-24 14:54:16 -0300

    Update MainAnimal.java

--- Lista06/src/ifsc/MainAnimal.java
+++ Lista06/src/ifsc/MainAnimal.java
@@ -17,5 +17,13 @@ public class MainAnimal extends Animal{} {
 	g.setNpatas(4);
 	g.setCor("Branco");
 	g.setEcossistema("desconhecido");
+	
+	for (int i = 0; i< Animal.size(); i++)
+			System.out.println(Animal.get(i).getNome());
+			System.out.println(Animal.get(i).getRaca());
+			System.out.println(Animal.get(i).getComprimento());
+			System.out.println(Animal.get(i).getNpatas());
+	                System.out.println(Animal.get(i).getCor());
+			System.out.println(Animal.get(i).getEcossistema());
 
 }
