9d919397ad275e1f77cdf514e47fa97dd9663c3a
Douglinhass <124809067+Douglinhass@users.noreply.github.com>
2023-05-24 14:50:49 -0300

    Add files via upload

---
+++ Lista06/src/ifsc/Animal.java
@@ -0,0 +1,79 @@
+package ifsc;
+
+public class Animal {
+
+		private String nome;
+		
+		private String raca;
+		
+		private f comprimento;
+		
+		public void setComprimento(f comprimento) {
+			this.comprimento = comprimento;
+		}
+
+		public Float getComprimento() {
+			return comprimento;
+		}
+
+		public void setComprimento(Float comprimento) {
+			this.comprimento = comprimento;
+		}
+
+		public Integer getNpatas() {
+			return npatas;
+		}
+
+		public void setNpatas(Integer npatas) {
+			this.npatas = npatas;
+		}
+
+		public String getCor() {
+			return cor;
+		}
+
+		public void setCor(String cor) {
+			this.cor = cor;
+		}
+
+		public String getEcossistema() {
+			return ecossistema;
+		}
+
+		public void setEcossistema(String ecossistema) {
+			this.ecossistema = ecossistema;
+		}
+
+		public Void getCaminha() {
+			return caminha;
+		}
+
+		public void setCaminha(Void caminha) {
+			this.caminha = caminha;
+		}
+
+		private Integer npatas;
+		
+		private String cor;
+		
+		private String ecossistema;
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getRaca() {
+			return raca;
+		}
+
+		public void setRaca(String raca) {
+			this.raca = raca;
+		}
+		
+		public Void caminha;
+
+	}
---
+++ Lista06/src/ifsc/MainAnimal.java
@@ -0,0 +1,21 @@
+package ifsc;
+
+public class MainAnimal extends Animal{} {
+	
+	Cachorro ca = new Cachorro();
+	ca.setNome("duquesa");
+	ca.setRaca("raca");
+	ca.setComprimento(30);
+	ca.setNpatas(4);
+	ca.setCor("Branco com marrom");
+	ca.setEcossistema("desconhecido");
+	
+    Gato g = new Gato();
+    g.setNome("mili");
+	g.setRaca("raca");
+	g.setComprimento(10);
+	g.setNpatas(4);
+	g.setCor("Branco");
+	g.setEcossistema("desconhecido");
+
+}
